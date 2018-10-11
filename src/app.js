03994bd32444defbbbfb44f5600e97965ad39d47
王迪 <wangdi05@yxqiche.com>
2018-10-11 15:35:42 +0800

    直接 mysql2 连接数据库

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
 const Koa = require('koa');
 
+const mysql = require('mysql2');
+const db_config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database:  'mydatabase'
+};
+const connection = mysql.createConnection(db_config);
+
+connection.connect(err => {
+    if (err) {
+        console.log('database connect fail, error:', err);
+        return;
+    }
+    console.log('database connect success');
+});
+
+const queryExpression1 = 'SELECT * FROM WEBSITES';
+
+connection.query(queryExpression1, function(err, res) {
+    if (err) {
+        throw err;
+    }
+});
+connection.end();
+
+
+
+
 const app = new Koa();
 
 
